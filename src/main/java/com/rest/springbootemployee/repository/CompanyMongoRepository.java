0a94bd6eab5f02e08645424890d52cdf18ec2318
WONGCH9 <wongch9@oocl.com>
2022-12-08 23:04:46 +0800

    feat: add should_get_all_companies_when_perform_get_given_two_employee

---
+++ src/main/java/com/rest/springbootemployee/repository/CompanyMongoRepository.java
@@ -0,0 +1,13 @@
+package com.rest.springbootemployee.repository;
+
+import com.rest.springbootemployee.entity.Company;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface CompanyMongoRepository extends MongoRepository<Company, String> {
+
+
+}
