697c0bd1fda3d7709249bfab1f65776d617a769c
tokaho <tokaho2000@gmail.com>
2022-12-08 19:16:10 +0800

    [Feat] delete company by id from mongodb

--- src/test/java/com/rest/springbootemployee/CompanyControllerTest.java
+++ src/test/java/com/rest/springbootemployee/CompanyControllerTest.java
@@ -153,7 +153,7 @@ public class CompanyControllerTest {
         employees.add(new Employee(String.valueOf(1), "lili", 20, "Female", 2000));
         employees.add(new Employee(String.valueOf(2), "coco", 10, "Female", 8000));
 
-        Company company = companyRepository.create(new Company("Spring", employees));
+        Company company = companyMongoRepository.save(new Company("Spring", employees));
 
         //when & then
         client.perform(MockMvcRequestBuilders.delete("/companies/{id}", company.getId()))
--- src/test/java/com/rest/springbootemployee/CompanyServiceTest.java
+++ src/test/java/com/rest/springbootemployee/CompanyServiceTest.java
@@ -135,7 +135,7 @@ public class CompanyServiceTest {
         companyService.delete(companyId);
 
         //then
-        verify(companyRepository).delete(companyId);
+        verify(companyMongoRepository).deleteById(companyId);
     }
 
     @Test
