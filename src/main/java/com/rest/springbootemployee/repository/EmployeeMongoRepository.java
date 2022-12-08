513d32b6291b2a049e2cbf51f1435970ec6f774a
lijo15 <lijo15@oocl.com>
2022-12-08 16:37:52 +0800

    feat: find by page

--- src/main/java/com/rest/springbootemployee/repository/EmployeeMongoRepository.java
+++ src/main/java/com/rest/springbootemployee/repository/EmployeeMongoRepository.java
@@ -4,6 +4,10 @@ import com.rest.springbootemployee.entity.Employee;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EmployeeMongoRepository extends MongoRepository<Employee, String> {
+//    List<Employee> findByGender(String gender);
+//    List<Employee> findByPage(int page, int pageSize);
 }
